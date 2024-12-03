c3fbaa3f8a0839fc16d295695feb804e8b7d48b3
Gilles Lassus <g.lassus@gmail.com>
2024-12-03 10:02:09 +0100

    maj

--- day03/code2.py
+++ day03/code2.py
@@ -1,23 +1,30 @@
 data = open('input.txt').read().splitlines()
 #data = open('input_test.txt').read().splitlines()
 
-t = "t_mul(542,5)+mmul(548,5mul(54,25)"
-t = data[0]
 
+flag_do = True
 
 def compte(t):
+    global flag_do
     s = 0
     i = 0
+
     while i < len(t):
+
         while i+4 < len(t) and t[i:i+4] != 'mul(':
+            if i+4 < len(t) and t[i:i+4] == 'do()':
+                flag_do = True
+            if i+7 < len(t) and t[i:i+7] == "don't()":
+                flag_do = False
             i += 1
+
         i += 4
         if i < len(t) and t[i].isdigit():
             n1_deb = i
             while i < len(t) and t[i].isdigit():
                 i += 1
             n1_end = i
-            #i += 1
+
             if i < len(t) and t[i] == ',':
                 i += 1
                 if i < len(t) and t[i].isdigit():
@@ -25,11 +32,12 @@ def compte(t):
                     while i < len(t) and t[i].isdigit():
                         i += 1
                     n2_end = i
-                    #i += 1
+                    
                     if i < len(t) and t[i] == ')':
                         n1 = int(t[n1_deb:n1_end])
                         n2 = int(t[n2_deb:n2_end])
-                        s += n1*n2
+                        if flag_do:
+                            s += n1*n2
     return s
 
 
